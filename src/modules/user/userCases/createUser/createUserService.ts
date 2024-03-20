fb1993b135b89fc64484d052d66db28396a89ebf
Esaú Bandeira <esaubandeira.dev@gmail.com>
2024-03-20 17:32:49 -0500

    feat: create user handle error

--- src/modules/user/userCases/createUser/createUserService.ts
+++ src/modules/user/userCases/createUser/createUserService.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from "tsyringe";
 import { UserRepository } from "../../repositories/implementations/UserRepository";
 import { IUserRepository } from "../../repositories/IUserRepository";
 import { ICreateUserDto } from "../../dtos/ICreateUserDto";
+import {hash} from 'bcrypt';
+import { AppError } from "../../../../shared/infra/http/errors/AppError";
 
 @injectable()
 class CreateUserService{
@@ -13,10 +15,11 @@ class CreateUserService{
   async execute(data:ICreateUserDto){
     const {name,email,cpf,birth_day,password} = data;
     const userAlreadyExists = await this.userRepository.findByEmail(email);
-
-    if(userAlreadyExists) return
-
-    await this.userRepository.create({name,email,cpf,birth_day,password})
+    if(userAlreadyExists?.cpf === cpf) throw new AppError('user already exist!', 409)
+    if(userAlreadyExists) throw new AppError('user already exist!', 409)
+    const salt = 8;
+    const passwordHash = await hash(password, salt);
+    await this.userRepository.create({name,email,cpf,birth_day,password: passwordHash})
   }
 
 }
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import 'express-async-errors'
 import router from './routes';
+import { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { AppError } from './errors/AppError';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
+
+    if(err instanceof AppError){
+      return response.status(err.statusCode).json({
+        message: err.message
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`
+    })
+
+  })
 
 export default app;
