8883d17a7dc152d275de897679b8d8110a0e7a61
Esaú Bandeira <esaubandeira.dev@gmail.com>
2024-03-20 22:10:26 -0500

    feat: cpf validation

--- src/modules/user/userCases/createUser/createUserService.ts
+++ src/modules/user/userCases/createUser/createUserService.ts
@@ -3,6 +3,7 @@ import { IUserRepository } from "../../repositories/IUserRepository";
 import { ICreateUserDto } from "../../dtos/ICreateUserDto";
 import {hash} from 'bcrypt';
 import { AppError } from "../../../../shared/infra/http/errors/AppError";
+import { cpf as CPF } from "cpf-cnpj-validator";
 
 @injectable()
 class CreateUserService{
@@ -13,6 +14,9 @@ class CreateUserService{
 
   async execute(data:ICreateUserDto):Promise<void>{
     const {name,email,cpf,birth_day,password} = data;
+    if (!CPF.isValid(cpf)) {
+      throw new AppError('CPF invalid', 400);
+    }
     const userAlreadyExists = await this.userRepository.findByEmail(email);
     if(userAlreadyExists?.cpf === cpf) throw new AppError('user already exist!', 409)
     if(userAlreadyExists) throw new AppError('user already exist!', 409)
