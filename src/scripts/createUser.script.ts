b2a321dd55a6df10ada8cf7bd9654bb68dab031c
Esaú Bandeira <esaubandeira.dev@gmail.com>
2024-03-27 02:53:40 -0500

    feat: added valid cpf in seed

--- src/scripts/createUser.script.ts
+++ src/scripts/createUser.script.ts
@@ -16,40 +16,131 @@ async function createAdminAndUsers() {
     console.log('------------------------------------------');
     console.log('Cannot create users because already exist');
     console.log('------------------------------------------');
-    return
+    throw new Error('Seed has already been executed')
   }
 
-  const userAdmin:ICreateUserDto = {
-    name: "Admin",
-    cpf: "44906858074",
-    birth_day: new Date(),
-    email: "admin@gmail.com",
-    password: hashSync("12345678",+process.env.SALT)
-  }
-
-  console.log('Creating a Admin user...');
-  const createUserAdmin = await userRepository.save(userAdmin)
-  console.log('Admin user created:', createUserAdmin);
-
-  for(let i = 1; i <= 10; i++){
-    let user:ICreateUserDto = {
-      name: `Test User${i}`,
-      cpf: `4490685807${i}`,
-      birth_day: new Date(`2000-01-${i < 10 ? '0' + i : i}`),
-      email: `test_user${i}@gmail.com`,
+  const users:ICreateUserDto[] = [
+    {
+      name: "Admin",
+      cpf: "44906858074",
+      birth_day: new Date(),
+      email: "admin@gmail.com",
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User1`,
+      cpf: `71589934008`,
+      birth_day: new Date(`2001-01-10}`),
+      email: `test_user1@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User2`,
+      cpf: `74443606009`,
+      birth_day: new Date(`2001-06-20}`),
+      email: `test_user2@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User3`,
+      cpf: `42607983088`,
+      birth_day: new Date(`2008-05-16}`),
+      email: `test_user3@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User4`,
+      cpf: `50849697034`,
+      birth_day: new Date(`2005-01-10}`),
+      email: `test_user4@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User5`,
+      cpf: `53056920086`,
+      birth_day: new Date(`2007-11-10}`),
+      email: `test_user5@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User6`,
+      cpf: `99665718096`,
+      birth_day: new Date(`2001-06-20}`),
+      email: `test_user6@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User7`,
+      cpf: `61247098052`,
+      birth_day: new Date(`2008-08-26}`),
+      email: `test_user7@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User8`,
+      cpf: `22192560060`,
+      birth_day: new Date(`2005-03-19}`),
+      email: `test_user8@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User9`,
+      cpf: `57942905012`,
+      birth_day: new Date(`2009-12-10}`),
+      email: `test_user9@gmail.com`,
+      password: hashSync("12345678",+process.env.SALT)
+    },
+    {
+      name: `Test User10`,
+      cpf: `035.480.330-16`,
+      birth_day: new Date(`2009-12-10}`),
+      email: `test_user10@gmail.com`,
       password: hashSync("12345678",+process.env.SALT)
     }
+
+]
+  console.log('------------------------------------------');
+  console.log('Creating a Admin user...');
+  console.log('------------------------------------------');
+
+  const createUserAdmin = await userRepository.save(users[0])
+
+  console.log('------------------------------------------');
+  console.log('Admin user created:', createUserAdmin);
+  console.log('------------------------------------------');
+
+  for(let i = 1; i <= users.length; i++){
+    console.log('------------------------------------------');
     console.log('creating users...');
-    const createUser = await userRepository.save(user)
+    console.log('------------------------------------------');
+
+    const createUser = await userRepository.save(users[i])
+
+    console.log('------------------------------------------');
     console.log('user created:', createUser);
+    console.log('------------------------------------------');
   }
 }
 
 initializeDataSources()
   .then(() => {
     createAdminAndUsers()
-    .then(()=>console.log('All users was created'))
-    .catch(err=>console.error('Error in creation users', err))
+    .then(()=>{
+      console.log('------------------------------------------');
+      console.log('------------------------------------------');
+      console.log('All users was created')
+      console.log('------------------------------------------');
+      console.log('------------------------------------------');
+
+    })
+    .catch(err=>{
+      console.log('------------------------------------------');
+      console.log('------------------------------------------');
+      console.error('something went wrong when creating users ====>', err)
+      console.log('------------------------------------------');
+      console.log('------------------------------------------');
+
+    })
     .finally(async()=>{
       await AppDataSource.destroy()
     })
